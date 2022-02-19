d743be1061e45eb79ba9422d9574468e53061afb
Mateus Elias <meap@cin.ufpe.br>
2022-02-19 18:42:01 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParse = require('body-parser')
+
+const userRoute = require('./routes/userRoute')
+
+const app = express()
+app.use(bodyParse.urlencoded({ extended: false}))
+const port = 3000
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Olá mundo, pelo Express!'))
+
+app.listen(port, () => console.log('API rodando na porta 3000'))
